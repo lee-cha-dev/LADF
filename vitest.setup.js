a35622f7e5d09937151790e740f5b25d8995a044
lee-cha-dev <leecharlesdev@gmail.com>
2026-02-11 00:43:38 -0600

    test expanded.

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom/vitest';
+import { vi } from 'vitest';
+
+vi.mock(
+  'xlsx',
+  () => ({
+    read: () => ({ SheetNames: [], Sheets: {} }),
+    utils: { sheet_to_json: () => [] },
+  }),
+  { virtual: true }
+);
